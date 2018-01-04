3d7ffcfa84de123fa072ab5c7144dc34fae52af5
Lidia Wisniewska <lidiawisnie@gmail.com>
2018-01-04 17:07:46 +0100

    Further css changes plus minor changes to candidates helper

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -1,6 +1,6 @@
 module CandidatesHelper
 	def candidate_avatar(candidate)
-		return '' if candidate.blank? || candidate.avatar.blank?
+		return 'n/a' if candidate.blank? || candidate.avatar.blank?
 		avatar = candidate.avatar
 		"<img class =\"avatar\" src = \"#{avatar}\">".html_safe
 	end
@@ -11,33 +11,33 @@ module CandidatesHelper
 		elsif candidate.role.present?
 			return candidate.role
 		else
-			return ''
+			return 'n/a'
 		end
 	end
 
 	def candidate_linkedin_url(candidate)
-		return '' unless candidate.linkedin.present?
+		return 'n/a' unless candidate.linkedin.present?
 		url = 'https://www.linkedin.com/'
 		handle = candidate.linkedin
 		social_url(handle, url)
 	end
 
 	def candidate_facebook_url(candidate)
-		return '' unless candidate.facebook.present?
+		return 'n/a' unless candidate.facebook.present?
 		url = 'https://www.facebook.com/'
 		handle = candidate.facebook
 		social_url(handle, url)
 	end
 
 	def candidate_twitter_url(candidate)
-		return '' unless candidate.twitter.present?
+		return 'n/a' unless candidate.twitter.present?
 		url = 'https://www.twitter.com/'
 		handle = candidate.twitter
 		social_url(handle, url)
 	end
 
 	def social_url(handle, url)
-		return '' if handle.blank?
+		return 'n/a' if handle.blank?
 		"<a href=\"#{url + handle}\" target=\"_blank\">#{handle}</a>".html_safe
 	end
 end
