196fa581ac4fec2320232ad643b7552f581ff25e
Lidia Wisniewska <lidiawisnie@gmail.com>
2018-02-18 14:58:00 +0100

    Go back to using papaerclip

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,8 +2,8 @@ class Candidate < ApplicationRecord
 	# before_save do
 	# 	self.enrichment = '' if enrichment.blank?
 	# end
-	# has_attached_file :cv
-	# validates_attachment_content_type :cv, content_type: ["application/pdf", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "text/plain"]
+	has_attached_file :cv
+	validates_attachment_content_type :cv, content_type: ["application/pdf", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "text/plain"]
 	belongs_to :user
 
 	after_create do
